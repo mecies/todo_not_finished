e36d3d295e4b126be28afabab90f75303ea8f60a
unknown <maciekhnat@gmail.com>
2018-12-15 11:51:41 +0100

    connected to mlab db

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
+const mongo = require('mongodb');
 const mongoose = require('mongoose');
 const passport = require('passport');
-
-mongoose.connect('mongodb://localhost/applogin', {useNewUrlParser: true})
+// login: maciek
+//password: todolist1
+mongoose.connect('mongodb://maciek:todolist1@ds123852.mlab.com:23852/todo-project', {
+    useNewUrlParser: true
+})
 .then(()=> console.log('connected to mongo db'))
 .catch(err => console.error('Could not connect to MongoDB', err));
 
@@ -20,7 +24,7 @@ const TestModel = mongoose.model('test', testSchema)
 
 //dodawanie lokalnie do mongo db
 async function addToDb(){
-    let test = new TestModel({name: 'i am the tester'})
+    let test = new TestModel({name: 'hejka ja marcines nowy w bazie'})
     test = await test.save();
 }
 
@@ -30,9 +34,9 @@ async function getTest(){
     
     console.log(test);
 }
-// addToDb();
+ addToDb();
 //branch maciej
-let i;
-getTest();
-app.listen(3000);
+
+//getTest();
+//app.listen(3000);
 
